a6fd1ed0617312b5ec265d9c87de385ae161c3a6
Gad Azogues <sistemas.gadazogues7@gmail.com>
2024-08-27 10:12:22 -0500

    feat: Add test for page not found

---
+++ tests/sections/not-found/page.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { render, screen } from "@testing-library/react";
+
+import { Page } from "../../../src/sections/not-found/page";
+
+ test("Not found component display header" , () => {
+   render(<Page />);
+
+  const heading = screen.getByText(/NotFound/i);
+
+  expect(heading).toBeInTheDocument();
+ });
