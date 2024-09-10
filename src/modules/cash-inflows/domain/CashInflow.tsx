f65d3a9a7b7f29e702c83f2c200a19dfbab8b6fb
Gad Azogues <sistemas.gadazogues7@gmail.com>
2024-09-10 11:07:01 -0500

    feat(cash-inflows): add nulls in types cash inflows

--- src/modules/cash-inflows/domain/CashInflow.tsx
+++ src/modules/cash-inflows/domain/CashInflow.tsx
@@ -2,23 +2,25 @@ export interface CashInflow {
   code: number
   concept: string
   reference: string
-  amountCollected: number
-  amountCollectedInWords: string
-  amount: number
-  amountInWords: string
-  interest: number
-  interestInWords: string
-  surcharges: number
-  surchargesInWords: string
-  change: number
-  changeInWords: string
-  totalToPay: number
-  totalInWords: string
-  paymentMethod: string
-  account: string
-  bank: string
-  check: string
-  notes: string
-  collector: string
-  invoice: number
+  amountCollected?: number
+  amountCollectedInWords?: string
+  amount?: number
+  amountInWords?: string
+  interest?: number
+  interestInWords?: string
+  surcharges?: number
+  surchargesInWords?: string
+  change?: number
+  changeInWords?: string
+  totalToPay?: number
+  totalInWords?: string
+  paymentMethod?: string
+  account?: string
+  bank?: string
+  check?: string
+  notes?: string
+  collector?: string
+  invoice?: number
+  requestId?: number
+  processUrl?: string
 }
