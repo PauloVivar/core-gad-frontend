eff083a4e5b044777e08969b4a2466effcbaa513
Gad Azogues <sistemas.gadazogues7@gmail.com>
2024-10-02 15:00:50 -0500

    feat: Add createdat date in column table

--- src/sections/transactions/columns.tsx
+++ src/sections/transactions/columns.tsx
@@ -1,6 +1,7 @@
 import { ColumnDef } from '@tanstack/react-table'
 
 import { Payment } from '@/modules/payment/domain/Payment'
+import { format } from 'date-fns'
 
 export const columns: ColumnDef<Payment>[] = [
   {
@@ -10,6 +11,17 @@ export const columns: ColumnDef<Payment>[] = [
   {
     accessorKey: 'value',
     header: 'Valor de pago'
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Fecha de transacción',
+    cell: ({ row }) => {
+      const formatedDate = format(
+        new Date(row.original.createdAt),
+        'dd/MM/yyyy HH:mm'
+      )
+      return formatedDate
+    }
   },
   {
     accessorKey: 'reference',
