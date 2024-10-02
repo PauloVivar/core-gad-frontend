448454e215b3e983b1a2300f75cd4ada9d3b70fe
Gad Azogues <sistemas.gadazogues7@gmail.com>
2024-10-02 15:01:42 -0500

    feat(navigation): Separeate list menu to navigation and add new link preguntas frecuentse

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -26,11 +26,11 @@ import {
   ArrowRightEndOnRectangleIcon,
   UserPlusIcon
 } from '@heroicons/react/24/outline'
+import { MenuList } from './Menu'
 
 const Navbar = () => {
   const { login, handlerLogout } = useAuth()
 
-  const commonClasses = 'flex items-center gap-3 rounded-lg px-3 py-2'
   const commonClassesMobile =
     'mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2'
 
@@ -53,157 +53,12 @@ const Navbar = () => {
               </Button>
             </div>
 
-            {/* Navbar Vertical */}
-            <div className="flex-1 bg-muted overflow-y-scroll">
-              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <HomeIcon className="h-4 w-4" />
-                  Inicio
-                  <Badge className="ml-auto flex h-5 w-5 shrink-0 items-center justify-center rounded-full">
-                    4
-                  </Badge>
-                </NavLink>
-                <NavLink
-                  to="/payments"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <ShoppingCartIcon className="h-4 w-4" />
-                  Consulta y Pago de Obligaciones
-                </NavLink>
-
-                <NavLink
-                  to="/procedures"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <InboxIcon className="h-4 w-4" />
-                  Tr&aacute;mites
-                </NavLink>
-
-                <NavLink
-                  to="/taxServices"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <UsersIcon className="h-4 w-4" />
-                  Servicios Tributarios
-                </NavLink>
-                <NavLink
-                  to="/citizenAttention"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <PresentationChartBarIcon className="h-4 w-4" />
-                  Atenci&oacute;n Ciudadana
-                </NavLink>
-                <NavLink
-                  to="/users"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <PresentationChartBarIcon className="h-4 w-4" />
-                  Usuarios{' '}
-                </NavLink>
-                <NavLink
-                  to="/titulos-de-credito"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <PresentationChartBarIcon className="h-4 w-4" />
-                  Deudas pendientes
-                </NavLink>
-                <NavLink
-                  to="/mis-transacciones"
-                  className={({ isActive }) =>
-                    `${commonClasses} ${
-                      isActive
-                        ? 'bg-muted text-primary transition-all hover:text-primary'
-                        : 'text-muted-foreground transition-all hover:text-primary'
-                    }`
-                  }
-                >
-                  <PresentationChartBarIcon className="h-4 w-4" />
-                  Mis transacciones
-                </NavLink>
-
-                {!login.isAdmin || (
-                  <>
-                    <NavLink
-                      to="/users/selectRegister"
-                      className={({ isActive }) =>
-                        `${commonClasses} ${
-                          isActive
-                            ? 'bg-muted text-primary transition-all hover:text-primary'
-                            : 'text-muted-foreground transition-all hover:text-primary'
-                        }`
-                      }
-                    >
-                      <PresentationChartBarIcon className="h-4 w-4" />
-                      Registar Usuarios{' '}
-                    </NavLink>
-
-                    <NavLink
-                      to="/terms"
-                      className={({ isActive }) =>
-                        `${commonClasses} ${
-                          isActive
-                            ? 'bg-muted text-primary transition-all hover:text-primary'
-                            : 'text-muted-foreground transition-all hover:text-primary'
-                        }`
-                      }
-                    >
-                      <PresentationChartBarIcon className="h-4 w-4" />
-                      T&eacute;rminos y Codiciones{' '}
-                    </NavLink>
-                  </>
-                )}
-              </nav>
-            </div>
+            <MenuList />
 
             {/* Card */}
-            <div className="mt-auto p-4">
-              <NewCard />
-            </div>
+            {/* <div className="mt-auto p-4"> */}
+            {/*   <NewCard /> */}
+            {/* </div> */}
           </div>
         </div>
       </div>
