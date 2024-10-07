4bc3328ac83b6c2523a00ec2fbcaadfebb41fc24
Gad Azogues <sistemas.gadazogues7@gmail.com>
2024-10-07 10:53:08 -0500

    test(payment state): add types to state last payment

--- src/sections/payments/state.ts
+++ src/sections/payments/state.ts
@@ -2,7 +2,15 @@ import { Payment } from '@/modules/payment/domain/Payment'
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
-export const useLastPaymentState = create(
+type State = {
+  lastPaymentPending: Payment | null
+}
+
+type Action = {
+  setLastPayment: (lastPaymentPending: Payment) => void
+}
+
+export const useLastPaymentState = create<State & Action>()(
   persist(
     (set) => ({
       lastPaymentPending: null,
